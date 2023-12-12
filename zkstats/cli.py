e9f557fddd4286710471f6076e181de616022f9d
mhchia <kevin.mh.chia@gmail.com>
2023-12-12 22:52:58 +0800

    verify proofs in prove

--- zkstats/cli.py
+++ zkstats/cli.py
@@ -1,42 +1,40 @@
 import os
 import click
 
-from .core import prover_gen_proof, prover_setup, load_model
+from .core import prover_gen_proof, prover_setup, load_model, verifier_verify
 
-os.makedirs(os.path.dirname('shared/'), exist_ok=True)
-os.makedirs(os.path.dirname('prover/'), exist_ok=True)
-verifier_model_path = os.path.join('shared/verifier.onnx')
-prover_model_path = os.path.join('prover/prover.onnx')
-verifier_compiled_model_path = os.path.join('shared/verifier.compiled')
-prover_compiled_model_path = os.path.join('prover/prover.compiled')
-pk_path = os.path.join('shared/test.pk')
-vk_path = os.path.join('shared/test.vk')
-proof_path = os.path.join('shared/test.pf')
-settings_path = os.path.join('shared/settings.json')
-srs_path = os.path.join('shared/kzg.srs')
-witness_path = os.path.join('prover/witness.json')
-# this is private to prover since it contains actual data
-data_path = os.path.join('data.json')
-comb_data_path = os.path.join('prover/comb_data.json')
+cwd = os.getcwd()
+# TODO: Should make this configurable
+output_dir = f"{cwd}/out"
+os.makedirs(output_dir, exist_ok=True)
+model_onnx_path = f"{output_dir}/model.onnx"
+compiled_model_path = f"{output_dir}/model.compiled"
+pk_path = f"{output_dir}/model.pk"
+vk_path = f"{output_dir}/model.vk"
+proof_path = f"{output_dir}/model.pf"
+settings_path = f"{output_dir}/settings.json"
+srs_path = f"{output_dir}/kzg.srs"
+witness_path = f"{output_dir}/witness.json"
+comb_data_path = f"{output_dir}/comb_data.json"
 
 
 @click.group()
 def cli():
     pass
 
+
 @click.command()
 @click.argument('model_path')
-def prove(model_path: str):
-    click.echo(f"Hello, {model_path}!")
-
-    prover_model = load_model(model_path)
-    print("!@# prover_model=", prover_model)
+@click.argument('data_path')
+def prove(model_path: str, data_path: str):
+    model = load_model(model_path)
+    print("Loaded model:", model)
     prover_setup(
         [data_path],
         comb_data_path,
-        prover_model,
-        prover_model_path,
-        prover_compiled_model_path,
+        model,
+        model_onnx_path,
+        compiled_model_path,
         "default",
         "resources",
         settings_path,
@@ -44,21 +42,29 @@ def prove(model_path: str):
         vk_path,
         pk_path,
     )
+    print("Finished setup")
     prover_gen_proof(
-        prover_model_path,
+        model_onnx_path,
         comb_data_path,
         witness_path,
-        prover_compiled_model_path,
+        compiled_model_path,
         settings_path,
         proof_path,
         pk_path,
         srs_path,
     )
+    print("Finished generating proof")
+    verifier_verify(proof_path, settings_path, vk_path, srs_path)
+    print("Proof path:", proof_path)
+    print("Settings path:", settings_path)
+    print("Verification key path:", vk_path)
+    print("SRS path:", srs_path)
 
 
 @click.command()
 def verify():
-    click.echo(f"Hello, verify!")
+    # TODO: Skip verify CLI in DataProvider for now. It's verified in `prove` already
+    raise NotImplementedError
 
 
 def main():
